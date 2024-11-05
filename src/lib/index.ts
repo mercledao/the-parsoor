c20562ddab32062b80a31cbf6724755c0e334bd0
Takshak Mudgal <takshakmudgal@gmail.com>
2024-11-06 01:38:36 +0530

    chore: update naming scheme

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 import { protocols } from "../config";
 import RhinoFi from "./RhinoFi";
-import UniswapV2 from "./Uniswap";
+import UniswapV2 from "./UniswapV2";
 
 export const parsers = {
   [protocols.rhinofi.identifier]: new RhinoFi(),
