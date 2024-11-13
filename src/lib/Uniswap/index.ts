ade65746da9fd630678a6a85b6e53ceaedee332c
Takshak Mudgal <takshakmudgal@gmail.com>
2024-11-14 01:37:11 +0530

    chore: remove unnessary comments

--- src/lib/Uniswap/index.ts
+++ src/lib/Uniswap/index.ts
@@ -23,12 +23,9 @@ export default class Uniswap implements IProtocolParserExport {
   ): Promise<ITransactionAction[]> {
     const actions: ITransactionAction[] = [];
 
-    // Check if transaction is valid
     if (!transaction?.to || !transaction?.data) {
       return actions;
     }
-
-    // Try V2 parsing first
     if (ProtocolHelper.txnToIsListenerContract(
       transaction,
       CONTRACT_ENUM.ROUTER_V2,
@@ -36,11 +33,10 @@ export default class Uniswap implements IProtocolParserExport {
     )) {
       const v2Actions = await UniswapParser.parseV2Transaction(transaction);
       if (v2Actions.length > 0) {
-        return v2Actions; // Return if V2 parsing successful
+        return v2Actions;
       }
     }
 
-    // Try V3 parsing if V2 failed
     const v3Actions = await UniswapParser.parseV3Transaction(transaction);
     if (v3Actions.length > 0) {
       return v3Actions;
