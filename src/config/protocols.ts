bc37f5b2fcfbdd42bda4ad2e5cf5be1da5033c55
Divyesh <99556535+divyesh87@users.noreply.github.com>
2024-11-16 20:51:34 +0530

    define odos in protcols

--- src/config/protocols.ts
+++ src/config/protocols.ts
@@ -1,16 +1,25 @@
-import { IProtocolDefinitionMap } from '../types';
+import { IProtocolDefinitionMap } from "../types";
 
 export const protocols: IProtocolDefinitionMap = {
   rhinofi: {
-    identifier: 'rhinofi',
-    name: 'RhinoFi',
-    twitter: 'https://x.com/rhinofi',
-    logo: 'https://assets.coingecko.com/coins/images/25169/large/rhinofi.png?1748888486'
+    identifier: "rhinofi",
+    name: "RhinoFi",
+    twitter: "https://x.com/rhinofi",
+    logo: "https://storage-mercle-prod.s3.amazonaws.com/public/protocol-logos/rhinologo.png",
+    website: "https://rhino.fi/",
   },
   uniswap: {
-    identifier: 'uniswap',
-    name: 'Uniswap',
-    twitter: 'https://x.com/Uniswap',
-    logo: 'https://assets.coingecko.com/coins/images/12504/large/uni.jpg?1600306401'
-  }
+    identifier: "uniswap",
+    name: "Uniswap",
+    twitter: "https://x.com/Uniswap",
+    logo: "https://storage-mercle-prod.s3.amazonaws.com/public/protocol-logos/uniswap.png",
+    website: "https://uniswap.org/",
+  },
+  odos: {
+    identifier: "odos",
+    name: "Odos",
+    twitter: "https://x.com/odosprotocol",
+    logo: "https://storage-mercle-prod.s3.amazonaws.com/public/protocol-logos/odos.png",
+    website: "https://www.odos.xyz/",
+  },
 };
