cd9104b70986aa05d3e444e1f9df1e13be85c63e
“Subham-Satapathy” <subhamsatapathy515@gmail.com>
2024-12-24 18:35:52 +0530

    test: add unit tests for DebridgeDepositParser and transaction parsing

---
+++ tests/lib/Debridge/index.test.ts
@@ -0,0 +1,49 @@
+import chalk from 'chalk';
+import { protocols } from '../../../src';
+import { ProtocolParserUtils } from '../../index';
+import { DEBRIDGE_VERSIONS, debridgePlaceOrderData, debridgerulfilledOrderData } from './data';
+
+describe('DebridgeDepositParser', () => {
+  let utils: ProtocolParserUtils;
+
+  beforeAll(async () => {
+    utils = new ProtocolParserUtils(protocols.debridge.identifier);
+    await utils.initialize();
+  });
+
+  it('is correctly defined', () => {
+    utils.isValidProtocol();
+  });
+
+  it('should parse place order transactions correctly', async () => {
+    const v1Transactions = debridgePlaceOrderData[DEBRIDGE_VERSIONS.V1];
+
+    for (const transaction of v1Transactions) {
+      
+      const actions = await utils.fetchAndParseTestTxn(transaction);
+      utils.assertTestTransactionForData(transaction, actions);
+
+      console.log(
+        chalk.green('Successfully parsed transaction with actions:', actions.map((action) => action.type).join(',')),
+        'and hash:',
+        transaction.txnHash
+      );
+    }
+  });
+
+  it('should parse fulfilled order transactions correctly', async () => {
+    const v1Transactions = debridgerulfilledOrderData[DEBRIDGE_VERSIONS.V1];
+
+    for (const transaction of v1Transactions) {
+      
+      const actions = await utils.fetchAndParseTestTxn(transaction);
+      utils.assertTestTransactionForData(transaction, actions);
+
+      console.log(
+        chalk.green('Successfully parsed transaction with actions:', actions.map((action) => action.type).join(',')),
+        'and hash:',
+        transaction.txnHash
+      );
+    }
+  });
+});
