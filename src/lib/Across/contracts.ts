98c42ba979f868716344bdc38e22136889bbbd69
“Subham-Satapathy” <subhamsatapathy515@gmail.com>
2024-12-28 20:45:58 +0530

    feat: add new chain IDs and corresponding contract addresses for ALEPH_ZERO, LISK, and READ_STONE

--- src/lib/Across/contracts.ts
+++ src/lib/Across/contracts.ts
@@ -39,6 +39,42 @@ const contracts: IProtocolContractDefinitions = {
       [CHAIN_ID.ZKSYNC_ERA]: {
         address: "0xE0B015E54d54fc84a6cB9B666099c46adE9335FF",
         listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+      },
+      [CHAIN_ID.ALEPH_ZERO]: {
+        address: "0x13fDac9F9b4777705db45291bbFF3c972c6d1d97",
+        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+      },
+      [CHAIN_ID.LINEA]: {
+        address: "0x7E63A5f1a8F0B4d0934B2f2327DAED3F6bb2ee75",
+        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+      },
+      [CHAIN_ID.LISK]: {
+        address: "0x9552a0a6624A23B848060AE5901659CDDa1f83f8",
+        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+      },
+      [CHAIN_ID.MODE]: {
+        address: "0x3baD7AD0728f9917d1Bf08af5782dCbD516cDd96",
+        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+      },
+      [CHAIN_ID.POLYGON]: {
+        address: "0x9295ee1d8C5b022Be115A2AD3c30C72E34e7F096",
+        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+      },
+      [CHAIN_ID.READ_STONE]: {
+        address: "0x13fDac9F9b4777705db45291bbFF3c972c6d1d97",
+        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+      },
+      [CHAIN_ID.SCROLL]: {
+        address: "0x3bad7ad0728f9917d1bf08af5782dcbd516cdd96",
+        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+      },
+      [CHAIN_ID.WORLDCHAIN]: {
+        address: "0x09aea4b2242abC8bb4BB78D537A67a245A7bEC64",
+        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+      },
+      [CHAIN_ID.ZORA]: {
+        address: "0x13fDac9F9b4777705db45291bbFF3c972c6d1d97",
+        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
       }
     },
 
