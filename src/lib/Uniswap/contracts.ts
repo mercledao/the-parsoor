df728cd4a00a0fd5dffaa2550813ffc63daa63f4
“Subham-Satapathy” <subhamsatapathy515@gmail.com>
2025-02-04 13:56:27 +0530

    refactor UniswapParser to enhance readability and maintainability by consolidating related functions

--- src/lib/Uniswap/contracts.ts
+++ src/lib/Uniswap/contracts.ts
@@ -125,193 +125,193 @@ export const contracts: IProtocolContractDefinitions = {
       },
     },
   },
-  [CONTRACT_ENUM.UNIVERSAL_ROUTER]: {
-    interface: new ethers.Interface([
-      ...UniswapUniversalRouterAbi,
-      "function execute(bytes commands, bytes[] inputs) payable",
-      "function execute(bytes commands, bytes[] inputs, uint256 deadline) payable",
-    ]),
-    deployments: {
-      [CHAIN_ID.ETHEREUM]: {
-        address: "0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.ARBITRUM]: {
-        address: "0x5E325eDA8064b456f4781070C0738d849c824258",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.BLAST]: {
-        address: "0x643770E279d5D0733F21d6DC03A8efbABf3255B4",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.OPTIMISM]: {
-        address: "0xCb1355ff08Ab38bBCE60111F1bb2B784bE25D7e8",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.POLYGON]: {
-        address: "0xec7BE89e9d109e7e3Fec59c222CF297125FEFda2",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.BASE]: {
-        address: "0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.BSC]: {
-        address: "0x4Dae2f939ACf50408e13d58534Ff8c2776d45265",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.AVALANCHE]: {
-        address: "0x4Dae2f939ACf50408e13d58534Ff8c2776d45265",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.ZKSYNC_ERA]: {
-        address: "0x28731BCC616B5f51dD52CF2e4dF0E78dD1136C06",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.CELO]: {
-        address: "0x643770E279d5D0733F21d6DC03A8efbABf3255B4",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.WORLDCHAIN]: {
-        address: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.ZORA]: {
-        address: "0x2986d9721A49838ab4297b695858aF7F17f38014",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-    },
-    events: {
-      [EVENT_ENUM.COMMAND_EXECUTED]: {
-        abi: new ethers.Interface([
-          "event CommandExecuted(bytes32 indexed commandHash, uint256 indexed commandType)",
-        ]),
-      },
-      [EVENT_ENUM.NATIVE_TRANSFER_RECEIVED]: {
-        abi: new ethers.Interface([
-          "event NativeTransferReceived(address indexed recipient, uint256 amount)",
-        ]),
-      },
-    },
-  },
-  [CONTRACT_ENUM.ROUTER_V3_01]: {
-    interface: new ethers.Interface(SwapRouter01Abi),
-    deployments: {
-      [CHAIN_ID.ARBITRUM]: {
-        address: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.ETHEREUM]: {
-        address: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.OPTIMISM]: {
-        address: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.POLYGON]: {
-        address: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-    },
-    events: {
-      [EVENT_ENUM.V3_SWAP]: {
-        abi: new ethers.Interface([
-          "event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)",
-        ]),
-      },
-    },
-  },
-  [CONTRACT_ENUM.ROUTER_V3_02]: {
-    interface: new ethers.Interface(SwapRouter02Abi),
-    deployments: {
-      [CHAIN_ID.AVALANCHE]: {
-        address: "0xbb00FF08d01D300023C629E8fFfFcb65A5a578cE",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.ETHEREUM]: {
-        address: "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.ARBITRUM]: {
-        address: "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.BSC]: {
-        address: "0xB971eF87ede563556b2ED4b1C0b0019111Dd85d2",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.BASE]: {
-        address: "0x2626664c2603336E57B271c5C0b26F421741e481",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.BLAST]: {
-        address: "0x549FEB8c9bd4c12Ad2AB27022dA12492aC452B66",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.CELO]: {
-        address: "0x5615CDAb10dc425a742d643d949a7F474C01abc4",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.OPTIMISM]: {
-        address: "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.POLYGON]: {
-        address: "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.WORLDCHAIN]: {
-        address: "0x091AD9e2e6e5eD44c1c66dB50e49A601F9f36cF6",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.ZKSYNC_ERA]: {
-        address: "0x99c56385daBCE3E81d8499d0b8d0257aBC07E8A3",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-      [CHAIN_ID.ZORA]: {
-        address: "0x7De04c96BE5159c3b5CeffC82aa176dc81281557",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-    },
-    events: {
-      [EVENT_ENUM.V3_SWAP]: {
-        abi: new ethers.Interface([
-          "event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)",
-        ]),
-      },
-    },
-  },
-  [CONTRACT_ENUM.LIMIT_ORDER_ROUTER]: {
-    interface: new ethers.Interface(LimitOrderRouterAbi),
-    deployments: {
-      [CHAIN_ID.ETHEREUM]: {
-        address: "0x00000011F84B9aa48e5f8aA8B9897600006289Be",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-    },
-    events: {
-      [EVENT_ENUM.LIMIT_ORDER_FILL]: {
-        abi: new ethers.Interface([
-          "event LimitOrderFilled(bytes32 indexed orderHash, address indexed maker, address inputToken, address outputToken, uint256 inputAmount, uint256 outputAmount, uint256 deadline)",
-        ]),
-      },
-    },
-  },
-  [CONTRACT_ENUM.EXCLUSIVE_LIMIT_ORDER_ROUTER]: {
-    interface: new ethers.Interface(LimitOrderRouterAbi),
-    deployments: {
-      [CHAIN_ID.ETHEREUM]: {
-        address: "0x6000da47483062A0D734Ba3dc7576Ce6A0B645C4",
-        listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
-      },
-    },
-    events: {
-      [EVENT_ENUM.LIMIT_ORDER_FILL]: {
-        abi: new ethers.Interface([
-          "event LimitOrderFilled(bytes32 indexed orderHash, address indexed maker, address inputToken, address outputToken, uint256 inputAmount, uint256 outputAmount, uint256 deadline)",
-        ]),
-      },
-    },
-  },
+  // [CONTRACT_ENUM.UNIVERSAL_ROUTER]: {
+  //   interface: new ethers.Interface([
+  //     ...UniswapUniversalRouterAbi,
+  //     "function execute(bytes commands, bytes[] inputs) payable",
+  //     "function execute(bytes commands, bytes[] inputs, uint256 deadline) payable",
+  //   ]),
+  //   deployments: {
+  //     [CHAIN_ID.ETHEREUM]: {
+  //       address: "0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.ARBITRUM]: {
+  //       address: "0x5E325eDA8064b456f4781070C0738d849c824258",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.BLAST]: {
+  //       address: "0x643770E279d5D0733F21d6DC03A8efbABf3255B4",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.OPTIMISM]: {
+  //       address: "0xCb1355ff08Ab38bBCE60111F1bb2B784bE25D7e8",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.POLYGON]: {
+  //       address: "0xec7BE89e9d109e7e3Fec59c222CF297125FEFda2",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.BASE]: {
+  //       address: "0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.BSC]: {
+  //       address: "0x4Dae2f939ACf50408e13d58534Ff8c2776d45265",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.AVALANCHE]: {
+  //       address: "0x4Dae2f939ACf50408e13d58534Ff8c2776d45265",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.ZKSYNC_ERA]: {
+  //       address: "0x28731BCC616B5f51dD52CF2e4dF0E78dD1136C06",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.CELO]: {
+  //       address: "0x643770E279d5D0733F21d6DC03A8efbABf3255B4",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.WORLDCHAIN]: {
+  //       address: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.ZORA]: {
+  //       address: "0x2986d9721A49838ab4297b695858aF7F17f38014",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //   },
+  //   events: {
+  //     [EVENT_ENUM.COMMAND_EXECUTED]: {
+  //       abi: new ethers.Interface([
+  //         "event CommandExecuted(bytes32 indexed commandHash, uint256 indexed commandType)",
+  //       ]),
+  //     },
+  //     [EVENT_ENUM.NATIVE_TRANSFER_RECEIVED]: {
+  //       abi: new ethers.Interface([
+  //         "event NativeTransferReceived(address indexed recipient, uint256 amount)",
+  //       ]),
+  //     },
+  //   },
+  // },
+  // [CONTRACT_ENUM.ROUTER_V3_01]: {
+  //   interface: new ethers.Interface(SwapRouter01Abi),
+  //   deployments: {
+  //     [CHAIN_ID.ARBITRUM]: {
+  //       address: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.ETHEREUM]: {
+  //       address: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.OPTIMISM]: {
+  //       address: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.POLYGON]: {
+  //       address: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //   },
+  //   events: {
+  //     [EVENT_ENUM.V3_SWAP]: {
+  //       abi: new ethers.Interface([
+  //         "event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)",
+  //       ]),
+  //     },
+  //   },
+  // },
+  // [CONTRACT_ENUM.ROUTER_V3_02]: {
+  //   interface: new ethers.Interface(SwapRouter02Abi),
+  //   deployments: {
+  //     [CHAIN_ID.AVALANCHE]: {
+  //       address: "0xbb00FF08d01D300023C629E8fFfFcb65A5a578cE",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.ETHEREUM]: {
+  //       address: "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.ARBITRUM]: {
+  //       address: "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.BSC]: {
+  //       address: "0xB971eF87ede563556b2ED4b1C0b0019111Dd85d2",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.BASE]: {
+  //       address: "0x2626664c2603336E57B271c5C0b26F421741e481",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.BLAST]: {
+  //       address: "0x549FEB8c9bd4c12Ad2AB27022dA12492aC452B66",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.CELO]: {
+  //       address: "0x5615CDAb10dc425a742d643d949a7F474C01abc4",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.OPTIMISM]: {
+  //       address: "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.POLYGON]: {
+  //       address: "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.WORLDCHAIN]: {
+  //       address: "0x091AD9e2e6e5eD44c1c66dB50e49A601F9f36cF6",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.ZKSYNC_ERA]: {
+  //       address: "0x99c56385daBCE3E81d8499d0b8d0257aBC07E8A3",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //     [CHAIN_ID.ZORA]: {
+  //       address: "0x7De04c96BE5159c3b5CeffC82aa176dc81281557",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //   },
+  //   events: {
+  //     [EVENT_ENUM.V3_SWAP]: {
+  //       abi: new ethers.Interface([
+  //         "event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)",
+  //       ]),
+  //     },
+  //   },
+  // },
+  // [CONTRACT_ENUM.LIMIT_ORDER_ROUTER]: {
+  //   interface: new ethers.Interface(LimitOrderRouterAbi),
+  //   deployments: {
+  //     [CHAIN_ID.ETHEREUM]: {
+  //       address: "0x00000011F84B9aa48e5f8aA8B9897600006289Be",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //   },
+  //   events: {
+  //     [EVENT_ENUM.LIMIT_ORDER_FILL]: {
+  //       abi: new ethers.Interface([
+  //         "event LimitOrderFilled(bytes32 indexed orderHash, address indexed maker, address inputToken, address outputToken, uint256 inputAmount, uint256 outputAmount, uint256 deadline)",
+  //       ]),
+  //     },
+  //   },
+  // },
+  // [CONTRACT_ENUM.EXCLUSIVE_LIMIT_ORDER_ROUTER]: {
+  //   interface: new ethers.Interface(LimitOrderRouterAbi),
+  //   deployments: {
+  //     [CHAIN_ID.ETHEREUM]: {
+  //       address: "0x6000da47483062A0D734Ba3dc7576Ce6A0B645C4",
+  //       listenForTransactions: [LISTEN_FOR_TRANSACTIONS.INCOMING],
+  //     },
+  //   },
+  //   events: {
+  //     [EVENT_ENUM.LIMIT_ORDER_FILL]: {
+  //       abi: new ethers.Interface([
+  //         "event LimitOrderFilled(bytes32 indexed orderHash, address indexed maker, address inputToken, address outputToken, uint256 inputAmount, uint256 outputAmount, uint256 deadline)",
+  //       ]),
+  //     },
+  //   },
+  // },
 };
