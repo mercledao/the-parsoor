d1ede8b4acea6db00f8f6e40159fd2eea82273b2
“Subham-Satapathy” <subhamsatapathy515@gmail.com>
2025-02-07 17:10:00 +0530

    feat: remove Oneinch protocol implementation and related files

--- src/config/protocols.ts
+++ src/config/protocols.ts
@@ -43,13 +43,6 @@ export const protocols: IProtocolDefinitionMap = {
     logo: "https://storage-mercle-prod.s3.amazonaws.com/public/protocol-logos/debridge.png",
     website: "https://app.debridge.finance/",
   },
-  oneinch: {
-    identifier: "oneinch",
-    name: "Oneinch",
-    twitter: "https://x.com/1inch",
-    logo: "https://storage-mercle-prod.s3.amazonaws.com/public/protocol-logos/oneinch.png",
-    website: "https://1inch.io/",
-  },
   paraswap: {
     identifier: "paraswap",
     name: "Paraswap",
