5f4dcb431400e6b841409dc848977405efd114b9
Divyesh <99556535+divyesh87@users.noreply.github.com>
2025-02-09 20:16:26 +0800

    add action and type tags

--- src/config/protocols.ts
+++ src/config/protocols.ts
@@ -1,4 +1,4 @@
-import { IProtocolDefinitionMap } from "../types";
+import { ActionTags, IProtocolDefinitionMap, ProtocolType } from "../types";
 
 export const protocols: IProtocolDefinitionMap = {
   rhinofi: {
@@ -7,6 +7,8 @@ export const protocols: IProtocolDefinitionMap = {
     twitter: "https://x.com/rhinofi",
     logo: "https://storage-mercle-prod.s3.amazonaws.com/public/protocol-logos/rhinologo.png",
     website: "https://rhino.fi/",
+    type: ProtocolType.CROSS_CHAIN_BRIDGE,
+    actionTags: [ActionTags.BRIDGE],
   },
   uniswap: {
     identifier: "uniswap",
@@ -14,6 +16,8 @@ export const protocols: IProtocolDefinitionMap = {
     twitter: "https://x.com/Uniswap",
     logo: "https://storage-mercle-prod.s3.amazonaws.com/public/protocol-logos/uniswap.png",
     website: "https://uniswap.org/",
+    type: ProtocolType.DEX,
+    actionTags: [ActionTags.SWAP],
   },
   lifi: {
     identifier: "lifi",
@@ -21,6 +25,8 @@ export const protocols: IProtocolDefinitionMap = {
     twitter: "https://x.com/lifiprotocol",
     logo: "https://storage-mercle-prod.s3.amazonaws.com/public/protocol-logos/lifi.png",
     website: "https://li.fi/",
+    type: ProtocolType.DEX_AGGREGGATOR,
+    actionTags: [ActionTags.SWAP, ActionTags.BRIDGE],
   },
   odos: {
     identifier: "odos",
@@ -28,6 +34,8 @@ export const protocols: IProtocolDefinitionMap = {
     twitter: "https://x.com/odosprotocol",
     logo: "https://storage-mercle-prod.s3.amazonaws.com/public/protocol-logos/odos.png",
     website: "https://www.odos.xyz/",
+    type: ProtocolType.DEX_AGGREGGATOR,
+    actionTags: [ActionTags.SWAP],
   },
   across: {
     identifier: "across",
@@ -35,6 +43,8 @@ export const protocols: IProtocolDefinitionMap = {
     twitter: "https://x.com/AcrossProtocol",
     logo: "https://storage-mercle-prod.s3.amazonaws.com/public/protocol-logos/across.png",
     website: "https://across.to/",
+    type: ProtocolType.CROSS_CHAIN_BRIDGE,
+    actionTags: [ActionTags.BRIDGE],
   },
   debridge: {
     identifier: "debridge",
@@ -42,6 +52,8 @@ export const protocols: IProtocolDefinitionMap = {
     twitter: "https://x.com/deBridgeFinance",
     logo: "https://storage-mercle-prod.s3.amazonaws.com/public/protocol-logos/debridge.png",
     website: "https://app.debridge.finance/",
+    type: ProtocolType.CROSS_CHAIN_BRIDGE,
+    actionTags: [ActionTags.BRIDGE],
   },
   aerodrome: {
     identifier: "aerodrome",
@@ -49,6 +61,8 @@ export const protocols: IProtocolDefinitionMap = {
     twitter: "https://x.com/AerodromeFi",
     logo: "https://storage-mercle-prod.s3.amazonaws.com/public/protocol-logos/aerodrome.png",
     website: "https://aerodrome.finance/",
+    type: ProtocolType.DEX,
+    actionTags: [ActionTags.SWAP],
   },
   pancakeswap: {
     identifier: "pancakeswap",
@@ -56,6 +70,8 @@ export const protocols: IProtocolDefinitionMap = {
     twitter: "https://x.com/PancakeSwap",
     logo: "https://storage-mercle-prod.s3.amazonaws.com/public/protocol-logos/pancakeswap.png",
     website: "https://pancakeswap.finance/",
+    type: ProtocolType.DEX,
+    actionTags: [ActionTags.SWAP],
   },
   cowswap: {
     identifier: "cowswap",
@@ -63,6 +79,8 @@ export const protocols: IProtocolDefinitionMap = {
     twitter: "https://x.com/CoWSwap",
     logo: "https://storage-mercle-prod.s3.amazonaws.com/public/protocol-logos/cowswap.png",
     website: "https://swap.cow.fi/",
+    type: ProtocolType.DEX_AGGREGGATOR,
+    actionTags: [ActionTags.SWAP],
   },
   paraswap: {
     identifier: "paraswap",
@@ -70,6 +88,8 @@ export const protocols: IProtocolDefinitionMap = {
     twitter: "https://x.com/parasvwap",
     logo: "https://storage-mercle-prod.s3.amazonaws.com/public/protocol-logos/paraswap.png",
     website: "https://www.paraswap.xyz/",
+    type: ProtocolType.DEX_AGGREGGATOR,
+    actionTags: [ActionTags.SWAP],
   },
   sushiswap: {
     identifier: "sushiswap",
@@ -77,6 +97,8 @@ export const protocols: IProtocolDefinitionMap = {
     twitter: "https://x.com/SushiSwap",
     logo: "https://storage-mercle-prod.s3.amazonaws.com/public/protocol-logos/sushiswap.png",
     website: "https://www.sushi.com/",
+    type: ProtocolType.DEX,
+    actionTags: [ActionTags.SWAP],
   },
   bungee: {
     identifier: "bungee",
@@ -84,5 +106,7 @@ export const protocols: IProtocolDefinitionMap = {
     twitter: "https://x.com/BungeeExchange",
     logo: "https://storage-mercle-prod.s3.amazonaws.com/public/protocol-logos/bungee.png",
     website: "https://www.bungee.exchange/",
+    type: ProtocolType.CROSS_CHAIN_BRIDGE,
+    actionTags: [ActionTags.BRIDGE],
   },
 };
